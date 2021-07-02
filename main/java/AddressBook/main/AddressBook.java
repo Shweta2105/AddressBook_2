fcaba75a3216b5d02b6b0235d721e1dd440368e3
Shweta2105 <sghadi21@gmail.com>
2021-07-02 08:07:09 +0530

    Welcome to addressBook

---
+++ main/java/AddressBook/main/AddressBook.java
@@ -0,0 +1,13 @@
+package AddressBook.main;
+
+/*
+@Author Shweta
+AddressBook system
+ */
+public class AddressBook {
+    public static void main(String[] args) {
+    //main method
+        System.out.println("------------------Welcome to AddressBook------------------");
+
+    }
+}
